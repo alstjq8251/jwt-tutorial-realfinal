c583b8b323968ff3a31d7162a46e2f0edbfa1706
JaejunSagong <jaejun7407@naver.com>
2022-08-05 12:54:57 +0900

    commit

---
+++ memoTproject/src/main/java/com/sparta/memoproject/model/Member.java
@@ -0,0 +1,35 @@
+package com.sparta.memoproject.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.sparta.memoproject.Timestamped;
+import lombok.*;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Getter
+@NoArgsConstructor
+@Table(name = "member")
+@Entity
+public class Member extends Timestamped {
+
+   @Id
+   @Column(name = "member_id")
+   @GeneratedValue(strategy = GenerationType.AUTO) //GenerationType.IDENTITY : ID값이 서로 영향없이 자기만의 테이블 기준으로 올라간다.
+   private Long id;
+   @Column(nullable = false, unique = true)
+   private String nickname;
+   @JsonIgnore
+   @Column(nullable = false)
+   private String password;
+
+   @Enumerated(EnumType.STRING)
+   private Authority authority;
+
+   @Builder
+   public Member(String nickname, String password, Authority authority) {
+      this.nickname = nickname;
+      this.password = password;
+      this.authority = authority;
+   }
+}
